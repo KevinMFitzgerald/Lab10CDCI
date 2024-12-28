80545b1e38deee73871ad8eee831302baffd7dd5
Kevcollector <kfitz2003@gmail.com>
2024-12-28 08:22:29 +0000

    Checking usesae

--- src/main/java/Atu/ie/PaymentController.java
+++ src/main/java/Atu/ie/PaymentController.java
@@ -1,24 +1,19 @@
 package Atu.ie;
 
 
-import org.springframework.web.bind.annotation.*;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/payment")
 public class PaymentController {
-    @PostMapping
+
+    @PostMapping()
     public String makePayment(@RequestBody Person person)
     {
-        return "got person "+person;
-
+        return "You called Microservice 2 - " + person;
     }
-    @GetMapping("/{employeeId}")
-    public String EmployeeId(@PathVariable String employeeId){
-        return "well done "+employeeId;
-
-    }
-
 }
-
-
-
--- src/main/java/Atu/ie/Person.java
+++ src/main/java/Atu/ie/Person.java
@@ -1,6 +1,5 @@
 package Atu.ie;
 
-
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
